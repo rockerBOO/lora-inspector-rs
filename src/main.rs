e3bbd810497ebc0e7a56801b27fe4229f4b11032
rockerBOO <rockerboo@gmail.com>
2023-11-25 16:33:08 -0500

    Add initial attempt at average mag and str

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
+use candle_core::{Device, Tensor};
 use memmap2::MmapOptions;
 use safetensors::SafeTensors;
 use std::fs::File;
-fn main() {
-    let filename = "64c92856.safetensors";
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let filename = "/mnt/900/training/sets/pov-2023-11-25-025803-4cf6f9ce/pov-2023-11-25-025803-4cf6f9ce.safetensors";
     let file = File::open(filename).unwrap();
     let buffer = unsafe { MmapOptions::new().map(&file).unwrap() };
 
@@ -16,6 +18,17 @@ fn main() {
     for (k, v) in metadata {
         println!("{k} {v}");
     }
+
+    let device = Device::Cpu;
+
+
+
+    let a = Tensor::randn(0f32, 1., (2, 3), &device)?;
+    let b = Tensor::randn(0f32, 1., (3, 4), &device)?;
+
+    let c = a.matmul(&b)?;
+    println!("{c}");
+    Ok(())
 
     // let tensors = SafeTensors::deserialize(&buffer).unwrap();
 }
