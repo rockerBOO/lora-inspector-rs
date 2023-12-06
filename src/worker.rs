c308a6cb835f132864b7eb2416d1bdd42aff9ee5
rockerBOO <rockerboo@gmail.com>
2023-12-06 04:02:35 -0500

    Add worker node system

--- src/worker.rs
+++ src/worker.rs
@@ -1,10 +1,10 @@
-use candle_core::{Tensor, WithDType};
+#![allow(unused)]
+extern crate console_error_panic_hook;
 use wasm_bindgen::prelude::*;
 
 use crate::inspector::LoRAInspector;
 use crate::metadata::Metadata;
-use crate::norms::{l1, l2, spectral};
-use crate::Error;
+// use crate::norms::{l1, l2, spectral};
 
 #[wasm_bindgen]
 pub struct LoraWorker {
@@ -13,8 +13,10 @@ pub struct LoraWorker {
     metadata: Metadata,
 }
 
+#[wasm_bindgen]
 impl LoraWorker {
-    pub fn new_from_buffer(buffer: &[u8], filename: String) -> Result<LoraWorker, Error> {
+    pub fn new_from_buffer(buffer: &[u8], filename: String) -> Result<LoraWorker, JsError> {
+        console_error_panic_hook::set_once();
         match LoRAInspector::new_from_buffer(buffer) {
             Ok(inspector) => match Metadata::new_from_buffer(buffer) {
                 Ok(metadata) => Ok(LoraWorker {
@@ -22,9 +24,11 @@ impl LoraWorker {
                     filename,
                     metadata,
                 }),
-                Err(e) => Err(Error::SafeTensor(e)),
+                Err(e) => Err(JsError::new("Could not load metadata")),
             },
-            Err(e) => Err(e),
+            Err(e) => Err(JsError::new(
+                &format!("Could not load inspector. {:?}", e).to_owned(),
+            )),
         }
     }
 
@@ -36,32 +40,32 @@ impl LoraWorker {
         self.filename
     }
 
-    pub fn norm<T: WithDType>(
-        self,
-        norm_fn: impl Fn(&Tensor) -> Result<T, candle_core::Error>,
-    ) -> Vec<T> {
-        self.inspector
-            .tensors()
-            .values()
-            .filter_map(|v| norm_fn(v).ok())
-            .collect()
-    }
+    // pub fn norm<T: WithDType>(
+    //     self,
+    //     norm_fn: impl Fn(&Tensor) -> Result<T, candle_core::Error>,
+    // ) -> Vec<T> {
+    //     self.inspector
+    //         .tensors()
+    //         .values()
+    //         .filter_map(|v| norm_fn(v).ok())
+    //         .collect()
+    // }
 }
 
-#[wasm_bindgen]
-pub fn l1_norm(worker: LoraWorker) -> Vec<f32> {
-    worker.norm(l1)
-}
-
-#[wasm_bindgen]
-pub fn l2_norm(worker: LoraWorker) -> Vec<f32> {
-    worker.norm(l2)
-}
-
-#[wasm_bindgen]
-pub fn spectral_norm(worker: LoraWorker) -> Vec<f32> {
-    worker.norm(spectral)
-}
+// #[wasm_bindgen]
+// pub fn l1_norm(worker: LoraWorker) -> Vec<f32> {
+//     worker.norm(l1)
+// }
+//
+// #[wasm_bindgen]
+// pub fn l2_norm(worker: LoraWorker) -> Vec<f32> {
+//     worker.norm(l2)
+// }
+//
+// #[wasm_bindgen]
+// pub fn spectral_norm(worker: LoraWorker) -> Vec<f32> {
+//     worker.norm(spectral)
+// }
 
 #[wasm_bindgen]
 pub fn metadata(worker: LoraWorker) -> Metadata {
