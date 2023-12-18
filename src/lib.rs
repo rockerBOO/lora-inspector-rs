c4a3b8dc164e0f2441adfc49d5d13793a17ffdfb
rockerBOO <rockerboo@gmail.com>
2023-12-17 23:18:29 -0500

    Add dims

--- src/lib.rs
+++ src/lib.rs
@@ -10,5 +10,35 @@ pub enum Error {
     Candle(candle_core::Error),
     SafeTensor(safetensors::SafeTensorError),
     Load(String),
-    Msg(String)
+    Msg(String),
+}
+
+pub fn get_base_name(name: &str) -> String {
+    name.split('.')
+        .filter(|part| !matches!(*part, "weight" | "lora_up" | "lora_down" | "alpha"))
+        .fold(String::new(), |acc, v| {
+            if acc.is_empty() {
+                v.to_owned()
+            } else {
+                format!("{acc}.{v}")
+            }
+        })
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::get_base_name;
+
+    #[test]
+    fn get_base_name_test() {
+        let base_name = get_base_name("lora_unet_up_blocks_1_attentions_1_proj_out.lora_up.weight");
+        assert_eq!(base_name, "lora_unet_up_blocks_1_attentions_1_proj_out");
+
+        let base_name =
+            get_base_name("lora_unet_up_blocks_1_attentions_1_proj_out.lora_down.weight");
+        assert_eq!(base_name, "lora_unet_up_blocks_1_attentions_1_proj_out");
+
+        let base_name = get_base_name("lora_unet_up_blocks_1_attentions_1_proj_out.alpha");
+        assert_eq!(base_name, "lora_unet_up_blocks_1_attentions_1_proj_out");
+    }
 }
