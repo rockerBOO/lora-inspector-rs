0fb9d7881d535eafadaa732488005550bbde5617
Dave Lage <rockerboo@gmail.com>
2026-04-06 00:19:58 -0400

    Add stylelint (#26)
    * Add stylelint
    
    * Add stylelint config file

--- crates/lora-inspector-wasm/assets/js/components/training/WaveletLossWeights.jsx
+++ crates/lora-inspector-wasm/assets/js/components/training/WaveletLossWeights.jsx
@@ -2,7 +2,7 @@ export function WaveletLossWeights({ weights }) {
 	return (
 		<div>
 			{Object.entries(weights).map(([k, v]) => (
-				<div className="weightedLoss" key={k}>
+				<div className="weighted-loss" key={k}>
 					<h4>{k}</h4>
 					<div>{v}</div>
 				</div>
