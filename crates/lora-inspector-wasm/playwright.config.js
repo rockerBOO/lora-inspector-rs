527a29c60701a3dd2479dcd42c755bbd8b62cef8
rockerBOO <rockerboo@gmail.com>
2026-04-13 12:01:29 -0400

    Fix playwright server

--- crates/lora-inspector-wasm/playwright.config.js
+++ crates/lora-inspector-wasm/playwright.config.js
@@ -83,7 +83,7 @@ export default defineConfig({
 
 	/* Run your local dev server before starting the tests */
 	webServer: {
-		command: "dev",
+		command: "yarn dev",
 		url: "http://localhost:5173",
 		reuseExistingServer: !process.env.CI,
 	},
